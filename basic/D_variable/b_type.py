dfd3bea46998f22e5aeb9e094acfb26a8cbaf824
libin <libin@mengxiang.com>
2026-02-06 14:06:20 +0800

    提交

---
+++ basic/D_variable/b_type.py
@@ -0,0 +1,28 @@
+x = 10 # 整数类型
+y = "张三" # 字符串类型
+o = 3.14 # 浮点数类型
+z = [1,2,3,4] # 数组(列表)类型 后面会讲到
+a = {'name':'李四','age':19} # 字典类型 后面会讲到
+b = {'北京','上海','广州','深圳'} # 集合类型 后面会讲到
+p = True # 布尔类型
+
+print(type(x))  # 输出：<class 'int'>，因为x是一个整数
+print(type(y))  # 输出：<class 'str'>，因为y是一个字符串
+print(type(o))  # 输出：<class 'float'>，因为o是一个浮点数
+print(type(z))  # 输出：<class 'list'>，因为z是一个数组
+print(type(a))  # 输出：<class 'dict'>，因为a是一个字典
+print(type(b))  # 输出：<class 'set'>，因为b是一个集合
+print(type(p))  # 输出：<class 'bool'>，因为p是一个布尔值
+
+
+
+
+
+
+
+
+
+
+
+
+
