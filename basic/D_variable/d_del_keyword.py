f4184c5277cd0102587773316dd7c2bdcc1cf78f
libin <libin@mengxiang.com>
2026-02-06 14:07:18 +0800

    提交

--- basic/D_variable/d_del_keyword.py
+++ basic/D_variable/d_del_keyword.py
@@ -2,5 +2,6 @@
 #我们可以使用del关键字删除对象的引用，但删除后再调用变量是会报错的，比如调用del a
 # 然后再去把a打印出来，会报这样的错误：NameError: name 'a' is not defined。
 a= 3
+#删除关键字
 del a
-a.bit_length()#NameError: name 'a' is not defined
+print(a) # name 'a' is not defined
