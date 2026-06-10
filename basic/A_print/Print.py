05530a5b2893e642b12c0245d76873687da98d19
libin <libin@mengxiang.com>
2026-06-10 15:09:18 +0800

    提交。

--- basic/A_print/Print.py
+++ basic/A_print/Print.py
@@ -1,13 +1,16 @@
 #输出
-print(520)
-print(95)
-#输出字符串print("字符串")
-#输出表达式
-print(3+1)
-#写入到文件
-# fp=open("E:/python/python/test.txt","a+")
-# print("helloword",file=fp)
-# fp.close()
-#不换行输出
-print("一","二","三")
-print("%s",1)
+# print("1")
+# print(520)
+# print(95)
+# #输出字符串print("字符串")
+# #输出表达式
+# print(3+1)
+# #写入到文件
+# # fp=open("E:/python/python/test.txt","a+")
+# # print("helloword",file=fp)
+# # fp.close()
+# #不换行输出
+# print("一","二","三")
+#打印
+fp = open("D:/python/python/test.txt")
+print("%s",1,3,sep=",",end=".",file=fp)
