bdda8fc650a4dedb74bb5f3c81f3cc55210099cc
libin <libin@mengxiang.com>
2026-06-11 11:16:58 +0800

    提交。

--- basic/K_list/A_list.py
+++ basic/K_list/A_list.py
@@ -9,6 +9,9 @@
 #1.事例
 #list = ['a','b','c',....]
 #index: 0-n-1:正向  -n->1:逆向索引。
+from typing import Any
+
+
 list_create = ['hello','world',98]
 print(id(list_create))#31949832
 print(type(list_create))#<class 'list'>
@@ -17,7 +20,7 @@ print(list_create)#['hello', 'world', 98]
 #2.创建列表
 #创建空列表
 list_empty = []
-list_obj_empty = list([])
+list_obj_empty = list[Any]([])
 print(list_empty)
 print(list_obj_empty)
 #创建有值列表
@@ -28,18 +31,4 @@ list_ele  = ['hello','world',9]
 list_obj_ele  = ['hello','world',9]
 print(list_ele)
 print(list_obj_ele)
-
-
-#列表的创建
-
-# #列表元素的获取
-# #通过index获取
-# a = list1[0]
-# print(a)
-# #通过元素获取指定位置,元素不存在，抛出异常
-# print(list1.index(9))
-# #从index 0--3位置查找'pad',查找不到,抛出异常
-# print(list1.index("pad",0,3))
-#
-#
 
