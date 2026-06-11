045d784f2336111f88b993a2ba7b3f56c5da19ec
libin <libin@mengxiang.com>
2026-06-11 11:41:44 +0800

    提交。

--- basic/K_list/F_any.py
+++ basic/K_list/F_any.py
@@ -3,4 +3,4 @@ my_list = ['apple', 'banana', 'cherry']
 print(any(x == 'apple' for x in my_list))
 # 包含元素大于2
 print(any(my_list.count('apple') > 1 for x in my_list))
-
+print(list(filter(lambda x:x=='apple',my_list)))
