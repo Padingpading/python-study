e47b5d64ccdd7dd7b86b3fc588dd9becb44d2752
libin <libin@mengxiang.com>
2026-07-30 20:26:54 +0800

    tj

---
+++ basic/P_func/参数传递/类型注解.py
@@ -0,0 +1,5 @@
+
+
+# 入参约束：整型、浮点型、字符串、布尔值
+def user_info(age: int, height: float, name: str, is_vip: bool) -> None:
+    print(name, age, height, is_vip)
---
+++ 类型注解(入参).py
@@ -0,0 +1,18 @@
+# 格式
+# def 函数名(参数1: 类型1, 参数2: 类型2 = 默认值) -> 返回值类型:
+#     函数逻辑
+
+#基础类型入参（int / float / str / bool）
+# 入参约束：整型、浮点型、字符串、布尔值
+def user_info(age: int, height: float, name: str, is_vip: bool) -> None:
+    print(name, age, height, is_vip)
+
+#带默认值入参
+def show_tip(tip: str = "操作成功") -> None:
+    print(tip)
+
+#复合类型入参（list / dict / tuple / set）
+# 列表、字典、元组、集合 入参注解
+def data_handle(nums: list[int], info: dict[str, str], pos: tuple[int,int], tags: set[str]) -> None:
+    print(nums, info, pos, tags)
+data_handle(nums=[1,2,3], info={"name":"张三"}, pos=(1,1), tags={"1","2","3"})
